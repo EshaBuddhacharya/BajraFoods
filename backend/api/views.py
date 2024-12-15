49f72a0172b92a11398afe6d09dbf5b77c85d1c9
Biraj Buddhacharya <otaku000god@gmail.com>
2024-12-15 11:03:23 +0545

    getFood api build

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import food
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializer import getFoodSerializer
 
-# Create your views here.
-def test(): 
-    pass
+@api_view(['GET'])
+def getFood(request): 
+    # getting all foods data from db 
+    foods = food.objects.all()
+    
+    # Turning python objects into json format 
+    serializer = getFoodSerializer(foods, many = True)
+    
+    # returning data
+    return Response(serializer.data)
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
    
     # dev installed apps  
     'api',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
